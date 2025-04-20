cbebbf932a27b33c253cadd5b31b3bc8a69d008a
HananB27 <hananbajramovic075@gmail.com>
2025-04-20 22:26:35 +0200

    adjust losses

--- Model/losses.py
+++ Model/losses.py
@@ -9,13 +9,13 @@ class HuberLoss:
         error = y_pred - y_true
         is_small_error = np.abs(error) <= self.delta
         squared_loss = 0.5 * error**2
-        linear_loss = self.delta * (np.abs(error) - 0.5 * self.delta)
-        return np.mean(np.where(is_small_error, squared_loss, linear_loss))
+        #sqrt_loss = self.delta * (np.abs(error) - 0.5 * self.delta)
+        sqrt_loss=np.sqrt(np.abs(error))          #not linear, sqrt loss
+        #sqrt_loss=np.power(np.abs(error),(1/2))
+        return np.mean(np.where(is_small_error, squared_loss, sqrt_loss))
 
     def backward(self, y_pred, y_true):
         error = y_pred - y_true
-        is_small_error = np.abs(error) <= self.delta
+        is_small_error = (np.abs(error) <= self.delta)
         dloss = np.where(is_small_error, error, self.delta * np.sign(error))
         self.dinputs = dloss / y_true.shape[0]  # Average over batch
-
-
